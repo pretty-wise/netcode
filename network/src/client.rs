52ed76c05471d0a450b90852fba874cfa5b27007
prettywise <krzysiek.stasik@gmail.com>
2021-01-10 14:45:00 -0500

    fix module namespacing

--- network/src/client.rs
+++ network/src/client.rs
@@ -1,67 +1,65 @@
-mod client {
-    use crate::socketio::*;
-    use std::mem::transmute;
-    use std::net::SocketAddr;
-    use std::str::FromStr;
+use crate::socketio;
+use std::mem::transmute;
+use std::net::SocketAddr;
+use std::str::FromStr;
 
-    pub struct NetcodeClient {
-        test: i32,
-        io: socketio::Context,
+pub struct NetcodeClient {
+    test: i32,
+    io: socketio::Context,
+}
+
+//3. DRY api for SocketIO to be used by the server
+
+#[no_mangle]
+pub extern "C" fn client_create() -> *mut NetcodeClient {
+    let local_addr = SocketAddr::from_str("127.0.0.1:0").unwrap();
+    let (socket_io, _port) = socketio::Context::new(local_addr);
+
+    let context = Box::new(NetcodeClient {
+        test: 2,
+        io: socket_io,
+    });
+
+    unsafe { transmute(context) }
+}
+
+#[no_mangle]
+pub extern "C" fn client_destroy(context: *mut NetcodeClient) {
+    let _dropped: Box<NetcodeClient> = unsafe { transmute(context) };
+}
+
+#[no_mangle]
+pub extern "C" fn client_update(context: *mut NetcodeClient) {
+    let client = unsafe { &mut *context };
+
+    client.test += 1;
+
+    loop {
+        match client.io.try_recv() {
+            Ok(data) => {
+                println!(
+                    "client read {}({}) on main. time since recv: {}ms",
+                    data.nbytes,
+                    data.buffer.len(),
+                    data.recv_time.elapsed().as_millis()
+                );
+                continue;
+            }
+            Err(_) => break,
+        };
     }
+}
 
-    //3. DRY api for SocketIO to be used by the server
-
-    #[no_mangle]
-    pub extern "C" fn client_create() -> *mut NetcodeClient {
-        let local_addr = SocketAddr::from_str("127.0.0.1:0").unwrap();
-        let (socket_io, _port) = socketio::Context::new(local_addr);
-
-        let context = Box::new(NetcodeClient {
-            test: 2,
-            io: socket_io,
-        });
-
-        unsafe { transmute(context) }
-    }
-
-    #[no_mangle]
-    pub extern "C" fn client_destroy(context: *mut NetcodeClient) {
-        let _dropped: Box<NetcodeClient> = unsafe { transmute(context) };
-    }
-
-    #[no_mangle]
-    pub extern "C" fn client_update(context: *mut NetcodeClient) {
-        let client = unsafe { &mut *context };
-
-        client.test += 1;
-
-        loop {
-            match client.io.try_recv() {
-                Ok(data) => {
-                    println!(
-                        "client read {}({}) on main. time since recv: {}ms",
-                        data.nbytes,
-                        data.buffer.len(),
-                        data.recv_time.elapsed().as_millis()
-                    );
-                    continue;
-                }
-                Err(_) => break,
-            };
-        }
-    }
-
-    #[cfg(test)]
-    mod tests {
-        use super::client_create;
-        use super::client_destroy;
-        use super::client_update;
-        #[test]
-        fn instatiation() {
-            let instance = client_create();
-            assert!(!instance.is_null());
-            client_update(instance);
-            client_destroy(instance);
-        }
+#[cfg(test)]
+mod tests {
+    use super::client_create;
+    use super::client_destroy;
+    use super::client_update;
+    #[test]
+    fn instatiation() {
+        let instance = client_create();
+        assert!(!instance.is_null());
+        client_update(instance);
+        client_destroy(instance);
     }
 }
--- network/src/socketio.rs
+++ network/src/socketio.rs
@@ -1,98 +1,96 @@
-pub mod socketio {
-    use std::net::{SocketAddr, UdpSocket};
-    use std::sync::mpsc;
-    use std::sync::mpsc::{Receiver, Sender};
-    use std::thread;
-    use std::time;
+use std::net::{SocketAddr, UdpSocket};
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::thread;
+use std::time;
 
-    use mpsc::TryRecvError;
+use mpsc::TryRecvError;
 
-    pub struct Packet {
-        pub recv_time: time::Instant,
-        pub nbytes: usize,
-        pub buffer: Vec<u8>,
-    }
+pub struct Packet {
+    pub recv_time: time::Instant,
+    pub nbytes: usize,
+    pub buffer: Vec<u8>,
+}
 
-    pub struct Context {
-        recv_thread: Option<thread::JoinHandle<()>>, // "the option dance"
-        read_rx: Receiver<Packet>,
-        socket: UdpSocket,
-        local_addr: SocketAddr, // used to send message to self to unblock the thread
-    }
+pub struct Context {
+    recv_thread: Option<thread::JoinHandle<()>>, // "the option dance"
+    read_rx: Receiver<Packet>,
+    socket: UdpSocket,
+    local_addr: SocketAddr, // used to send message to self to unblock the thread
+}
 
-    impl Context {
-        pub fn new(mut local_addr: SocketAddr) -> (Context, u16) {
-            let socket = UdpSocket::bind(local_addr).unwrap();
-            local_addr = socket.local_addr().unwrap();
-            let recv_socket = socket.try_clone().unwrap();
+impl Context {
+    pub fn new(mut local_addr: SocketAddr) -> (Context, u16) {
+        let socket = UdpSocket::bind(local_addr).unwrap();
+        local_addr = socket.local_addr().unwrap();
+        let recv_socket = socket.try_clone().unwrap();
 
-            let (tx, rx): (Sender<Packet>, Receiver<Packet>) = mpsc::channel();
+        let (tx, rx): (Sender<Packet>, Receiver<Packet>) = mpsc::channel();
 
-            let thread = thread::spawn(move || {
-                loop {
-                    let mut buffer = vec![0; 1500];
-                    match recv_socket.recv_from(&mut buffer.as_mut_slice()) {
-                        Ok((nbytes, src_addr)) => {
-                            let recv_time = time::Instant::now();
-                            if nbytes == 1 {
-                                break; // see a note in client_destroy.
-                            }
-                            buffer.resize(nbytes, 0);
-                            println!("received {} bytes from {}", nbytes, src_addr);
-                            tx.send(Packet {
-                                nbytes,
-                                recv_time,
-                                buffer,
-                            })
-                            .unwrap();
+        let thread = thread::spawn(move || {
+            loop {
+                let mut buffer = vec![0; 1500];
+                match recv_socket.recv_from(&mut buffer.as_mut_slice()) {
+                    Ok((nbytes, src_addr)) => {
+                        let recv_time = time::Instant::now();
+                        if nbytes == 1 {
+                            break; // see a note in client_destroy.
                         }
-                        Err(_) => break,
+                        buffer.resize(nbytes, 0);
+                        println!("received {} bytes from {}", nbytes, src_addr);
+                        tx.send(Packet {
+                            nbytes,
+                            recv_time,
+                            buffer,
+                        })
+                        .unwrap();
                     }
+                    Err(_) => break,
                 }
-            });
+            }
+        });
 
-            let local_port = local_addr.port();
-            (
-                Context {
-                    recv_thread: Some(thread),
-                    read_rx: rx,
-                    socket,
-                    local_addr,
-                },
-                local_port,
-            )
-        }
-
-        pub fn send(&self, buf: &[u8], dest: SocketAddr) -> std::io::Result<usize> {
-            self.socket.send_to(buf, dest)
-        }
-
-        pub fn try_recv(&self) -> Result<Packet, TryRecvError> {
-            self.read_rx.try_recv()
-        }
+        let local_port = local_addr.port();
+        (
+            Context {
+                recv_thread: Some(thread),
+                read_rx: rx,
+                socket,
+                local_addr,
+            },
+            local_port,
+        )
     }
 
-    impl Drop for Context {
-        fn drop(&mut self) {
-            // note(kstasik):
-            // 1 bytes is sent from the main thread when terminating library as rust exposes no way to close a socket.
-            let empty = [0; 1];
-            self.send(&empty, self.local_addr).unwrap();
-            self.recv_thread.take().unwrap().join().unwrap();
-        }
+    pub fn send(&self, buf: &[u8], dest: SocketAddr) -> std::io::Result<usize> {
+        self.socket.send_to(buf, dest)
+    }
+
+    pub fn try_recv(&self) -> Result<Packet, TryRecvError> {
+        self.read_rx.try_recv()
+    }
+}
+
+impl Drop for Context {
+    fn drop(&mut self) {
+        // note(kstasik):
+        // 1 bytes is sent from the main thread when terminating library as rust exposes no way to close a socket.
+        let empty = [0; 1];
+        self.send(&empty, self.local_addr).unwrap();
+        self.recv_thread.take().unwrap().join().unwrap();
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::socketio;
+    use super::Context;
     use core::panic;
     use std::{net::SocketAddr, str::FromStr};
 
     #[test]
     fn lifetime() {
         let addr = SocketAddr::from_str("127.0.0.1:8888").unwrap();
-        let (_context, port) = socketio::Context::new(addr);
+        let (_context, port) = Context::new(addr);
         println!("socket opened on port: {}", port);
     }
 
@@ -100,7 +98,7 @@ mod tests {
     fn messaging() {
         let hostname = [127, 0, 0, 1];
         let mut addr = SocketAddr::from((hostname, 0));
-        let (context, port) = socketio::Context::new(addr);
+        let (context, port) = Context::new(addr);
         println!("socket opened on port: {}", port);
 
         addr = SocketAddr::from((hostname, port));
